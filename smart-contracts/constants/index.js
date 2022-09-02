178e118beacc112c5eb6a8c60dc797c7e731abb8
Luis <luiscarlossfaria@gmail.com>
2022-09-02 15:43:50 -0300

    Initial

---
+++ smart-contracts/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x01f2707c796cEA0cF61f1e639134b69AB41a301B";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
